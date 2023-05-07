400d067cd222234b1451c6cdb3d7bb22fccb8039
iamjc93 <jan@goacquirely.com>
2023-05-08 07:50:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Your-Links-extension
+<p align = "center">
+  <img width = "350" src="images/Your-Links.png">
+</p>
+
+Your-Links is a Chrome extension that allows you to save your favorite links and taps in a simple and easy way.
+
+## Installation
+
+- First clone the repository
+    
+    ```bash
+    git clone https://github.com/efraimnabil/Your-Links-extension.git
+    ```
+
+- Then open the extension page in chrome by typing `chrome://extensions/` in the address bar.
+
+- Enable the developer mode in the top right corner.
+
+- Click on `Load unpacked` and select the folder of the project.
+
+- Now you can use the extension.
+
+## Usage
+
+Click on the extension icon to open the extension
+
+- Type a link and click `Save` to save it.
+
+- Click on `Save Tap` to save the current tab.
+
+- Click on `Delete All` to delete all the saved links.
+
+- Click on `Delete` to delete a specific link.
+
+- Click on `rename` to rename a specific link.
---
+++ script.js
@@ -0,0 +1,132 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const listEl = document.getElementById('ul-el');
+    const saveBtn = document.getElementById('save-btn');
+
+    let linksFromLocalStorage = JSON.parse(localStorage.getItem('links'));
+    let links = [];
+
+    if (linksFromLocalStorage) {
+        links = linksFromLocalStorage;
+        renderLinks();
+    }
+    links = unique(links);
+
+    function unique(arr) {
+        let result = [];
+        for (let str of arr) {
+            let isUnique = true;
+            for (let el of result) {
+                if (str[0] === el[0]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                result.push(str);
+            }
+        }
+        return result;
+    }
+
+    function renderLinks() {
+        let listItems = '';
+        links = unique(links);
+        for (let i = 0; i < links.length; i++) {
+            listItems += `
+            <li>
+            <a
+            href = "${links[i][0]}" target = "_blank"> <span class= "title">${links[i][2]}</span>
+            <span class = "url"> ${links[i][0]}</span>
+            </a>
+            </li>
+        `;
+        }
+        listEl.innerHTML = listItems;
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let url = tabs[0].url;
+        let webTitle = tabs[0].title;
+        const regex = /^https:\/\/([\w.-]+)\/([\w-]+)-episode-(\d+)\.html$/;
+
+        const match = regex.exec(url);
+
+        if (match !== null) {
+
+            const domain = match[1];
+            const title = match[2];
+            const episodeNumber = match[3];
+            if (domain === 'www1.watchasian.id') {
+
+                console.log(url); // "https://www1.watchasian.id/may-i-help-you-2022-episode-9.html"
+                console.log(domain); // "www1.watchasian.id"
+                console.log(webTitle); // "Watch May I Help You (2022) Episode 9 Online With English sub,FullHD | Dramacool"
+                console.log(title); // "may-i-help-you-2022"
+                console.log(episodeNumber); // "9"
+                console.log(tabs); // "9"
+
+                const index = links.findIndex(elem => elem[2] === title); // find the item with title
+                if (index !== -1) {
+                    links.splice(index, 1);
+                }
+
+                links.push([url, webTitle, title, episodeNumber]);
+
+                localStorage.setItem('links', JSON.stringify(links));
+                // Save links to links.json file
+                // saveLinksToFileNew(links);
+            }
+        } else {
+            console.log("No match found.");
+        }
+
+        renderLinks();
+    });
+
+    // Function to save links to links.json file
+    function saveLinksToFileNew(links) {
+        const data = JSON.stringify(links, null, 2);
+        const blob = new Blob([data], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'links.json';
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
+    saveBtn.addEventListener('click', async () => {
+        const linksFromLocalStorage = JSON.parse(localStorage.getItem('links'));
+        // const fileHandle = await window.showSaveFilePicker();
+        // const writable = await fileHandle.createWritable();
+        // await writable.write(JSON.stringify(linksFromLocalStorage));
+        // await writable.close();
+
+        saveLinksToFile(linksFromLocalStorage)
+    });
+
+    async function saveLinksToFile(links) {
+        try {
+            const fileHandle = await window.showSaveFilePicker({
+                suggestedName: 'links.json',
+                types: [
+                    {
+                        description: 'JSON Files',
+                        accept: {
+                            'application/json': ['.json'],
+                        },
+                    },
+                ],
+            });
+
+            const writable = await fileHandle.createWritable();
+            await writable.write(JSON.stringify(links, null, 2));
+            await writable.close();
+            console.log('Links saved to file.');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
