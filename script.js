1ed245d7825b33e6c4609d3a7c8ad1a5f23bae33
iamjc93 <jan@goacquirely.com>
2023-12-05 18:11:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', async function () {
+
+    const apiURL = 'http://127.0.0.1:8001/api/v1/public/visited-websites';
+
     const listEl = document.getElementById('ul-el');
     let listItems = '';
-
     let linksFromLocalStorage = await fetchData();
 
     const regex = /^https:\/\/([\w.-]+)\/([\w-]+)-episode-(\d+)\.html$/;
 
     linksFromLocalStorage.map((link) => {
+        console.log(link);
         const match = regex.exec(link.url);
-        console.log(match);
 
         listItems += `
             <li class="list-group-item">
-                <div class="card" id="${match[2]}">
+                <div class="card bg-success-subtle" id="${match[2]}">
                     <div class="card-body">
                         <h5 class="card-title">${match[2]}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Episode ${match[3]}</h6>
@@ -28,10 +30,22 @@ document.addEventListener('DOMContentLoaded', async function () {
     listEl.innerHTML = listItems;
 
     async function fetchData() {
-        const apiURL = 'http://127.0.0.1:8001/api/v1/public/visited-websites';
-
         const data = await fetch(apiURL);
         const json = await data.json();
+
+        return json;
+    }
+
+    async function update(userId, url, isCompleted) {
+        await fetch(apiURL, {
+            method: 'PUT',
+            data: JSON.stringify({
+                userId: userId,
+                url: url,
+                isCompleted: isCompleted,
+                isEnable: true,
+            })
+        });
 
         return json;
     }
